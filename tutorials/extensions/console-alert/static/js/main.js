d04f38c62d4131da38d3e624a66d18beff1f0788
Carson <jackparadise520a@gmail.com>
2021-07-12 18:07:14 +0800

    Teach you how to debug for the chrome extension. (#2)

---
+++ tutorials/extensions/console-alert/static/js/main.js
@@ -0,0 +1,22 @@
+(()=>{
+  chrome.commands.onCommand.addListener((cmdName) => {
+    switch (cmdName) {
+      case "show-alert":
+        chrome.storage.sync.set({msg: cmdName}) // You can not get the context on the function, so using the Storage API to help you. // https://developer.chrome.com/docs/extensions/reference/storage/
+        chrome.tabs.query({active: true, currentWindow: true}).then(([tab])=>{
+          chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            function: () => {
+              chrome.storage.sync.get(['msg'], ({msg})=> {
+                console.log(`${msg}`)
+                alert(`Command: ${msg}`)
+              })
+            }
+          })
+        })
+        break
+      default:
+        alert(`Unknown Command: ${cmdName}`)
+    }
+  })
+})()
