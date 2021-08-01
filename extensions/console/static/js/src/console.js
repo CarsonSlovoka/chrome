455e04ef415e31c90aae29ee4492176ec1fa6e09
Carson <jackparadise520a@gmail.com>
2021-08-01 22:40:09 +0800

    Redesign the CommandCenter to make it more readable. (#5)

--- extensions/console/static/js/src/console.js
+++ extensions/console/static/js/src/console.js
@@ -1,4 +1,13 @@
 import * as MathEval from "../pkg/math/math-eval.js"
+import {ArgumentParser} from "../pkg/flag/argument-parser.js"
+
+
+function Cmd(name, aliases = undefined, description, func = undefined) {
+  this.name = name
+  this.aliases = aliases ?? [name]
+  this.description = description
+  this.func = func
+}
 
 class CommandCenter {
   /**
@@ -6,37 +15,37 @@ class CommandCenter {
    * */
   constructor(node) {
     this.node = node
-    this.cmdTable = {
-      list: {
-        aliases: ["list", "ls"],
-        description: "Show the information of each tab.",
-        func: () => this.list()
-      },
-      cls: {
-        aliases: ["cls"],
-        description: "<b>Clear</b> screen",
-        func: () => this.cls()
-      },
-      help: {
-        aliases: ["help"],
-        description: "Show all command lists.",
-        func: () => this.help()
-      }
+    this.cmdArray = [
+      new Cmd("list", ["list", "ls"], "Show the information of each tab.", () => this.list()),
+      new Cmd("cls", ["cls"], "<b>Clear</b> screen", () => this.cls()),
+      new Cmd("help", ["help", "h"], "Show all command lists.", () => this.help()),
+      new Cmd("=", ["="], "Do Arithmetic",
+        (expression) => {
+          const argObj = ArgumentParser(expression)
+          if (argObj === undefined) {
+            return this.DoArithmetic(expression.replaceAll(" ", ""))
+          }
+          if (argObj.help || argObj.h) {
+            this.addElem("((1+e)*3/round{3.5})%2", "p", {})
+          }
+        }
+      ),
+    ]
+  }
+
+  addElem(text, elem = "p", {className = undefined, needAppend = true}) {
+    const range = document.createRange()
+    const frag = range.createContextualFragment(`<${elem} ${className !== undefined ? 'class="' + className + '"' : ""}>${text}</${elem}>`)
+    if (needAppend) {
+      this.node.append(frag)
+    } else {
+      return frag // If the frag has appended, then childNodes will empty.
     }
   }
 
-  addP(text) {
-    const range = document.createRange()
-    const frag = range.createContextualFragment(`<p>${text}</p>`)
-    this.node.append(frag)
-  }
-
   addA(text, href, windowId, favIconSRC) {
-    const range = document.createRange()
-
-    const frag = range.createContextualFragment(`<div class="mt-3">
-<img class="me-2 bg-white" src="${favIconSRC}" alt="" style="max-width: 32px; max-height:32px"/><a tabindex="0" class="text-decoration-none">${text}</a>
-</div>`)
+    const frag = this.addElem(`<img class="me-2 bg-white" src="${favIconSRC}" alt="" style="max-width: 32px; max-height:32px"/><a tabindex="0" class="text-decoration-none">${text}</a>`,
+      "div", {className: "mt-3", needAppend: false})
 
     const a = frag.querySelector('a')
     // a.href = href
@@ -58,8 +67,8 @@ class CommandCenter {
       windowArray.forEach(item => {
         item.tabs.sort( // Let the same icons be arranged together.
           (a, b) => {
-            a.favIconUrl = a.favIconUrl?? ""
-            b.favIconUrl = b.favIconUrl?? ""
+            a.favIconUrl = a.favIconUrl ?? ""
+            b.favIconUrl = b.favIconUrl ?? ""
             return a.favIconUrl.localeCompare(b.favIconUrl)
           }
         ).forEach(tab => { // sort((tab)=>tab.favIconUrl)
@@ -89,14 +98,14 @@ class CommandCenter {
     <thead>
       <tr>
         <th>Desc.</th>
-        <th>Hotkey</th>
+        <th>Available commands</th>
       </tr>
     </thead>
     <tbody></tbody>
     </table>
     `)
     const tbody = fragTable.querySelector(`tbody`)
-    for (const [name, item] of Object.entries(this.cmdTable)) {
+    for (const cmd of this.cmdArray) {
       /* doesn't work https://stackoverflow.com/q/43102944/9935654
       const trFrag = document.createRange().createContextualFragment(`<th>
 <td>${item.description}</td>
@@ -106,10 +115,10 @@ class CommandCenter {
       const tr = document.createElement("tr")
       const tdDesc = document.createElement("td")
       const tdHotkey = document.createElement("td")
-      tdDesc.innerHTML = item.description
+      tdDesc.innerHTML = cmd.description
 
       let hotkeyString = ""
-      item.aliases.forEach(aliases => hotkeyString += `<kbd>${aliases}</kbd>  `) // <code>
+      cmd.aliases.forEach(aliases => hotkeyString += `<kbd>${aliases}</kbd>  `) // <code>
       tdHotkey.innerHTML = hotkeyString // hotkeyString.slice(0, -1) // remove last character
       tr.append(tdDesc, tdHotkey)
       tbody.append(tr)
@@ -118,11 +127,11 @@ class CommandCenter {
   }
 
   DoArithmetic(expressions) {
-    this.addP(`${MathEval.Parse(expressions)}`)
-  }
-
-  RunCmd(cmdName) {
-    this.cmdTable[cmdName].func()
+    try {
+      this.addElem(`${MathEval.Parse(expressions)}`, "p", {})
+    } catch (e) {
+      this.ShowErrMsg(`${e.messageType} ${e.message}`, "❌")
+    }
   }
 
   /**
@@ -145,13 +154,10 @@ class CommandCenter {
 
 (() => {
   window.onload = () => {
+    // Obj
     const cmdObj = new CommandCenter(document.querySelector(`section[data-name="message"]`))
 
-    const validCmdNameList = []
-    for (const [name, item] of Object.entries(cmdObj.cmdTable)) {
-      item.aliases.forEach(aliases => validCmdNameList.push({name, aliases}))
-    }
-
+    // DOM
     const input = document.querySelector(`input`)
     const commitBtn = document.querySelector(`button`)
     input.addEventListener("keyup", (keyboardEvent) => {
@@ -164,22 +170,20 @@ class CommandCenter {
     })
 
     commitBtn.onclick = (event) => {
-      // const cmdNameArray = Object.getOwnPropertyNames(cmdTable)
       const inputValue = input.value
 
-      if (inputValue.startsWith("=")) {
-        cmdObj.DoArithmetic(inputValue.slice(1))
-        return
+      for (const cmd of cmdObj.cmdArray) {
+        if (cmd.aliases.find(aliases => inputValue.startsWith(aliases)) === undefined) {
+          continue
+        }
+        if (cmd.func !== undefined) {
+          cmd.func(inputValue.slice(cmd.aliases.length))
+          input.value = ""
+          return
+        }
       }
 
-      const cmdItem = validCmdNameList.find(({name, aliases}) => aliases === inputValue)
-      if (cmdItem === undefined) {
-        cmdObj.ShowErrMsg(`Unknown command: ${inputValue}`, "❌")
-        return
-      }
-
-      cmdObj.RunCmd(cmdItem.name)
-      input.value = ""
+      cmdObj.ShowErrMsg(`Unknown command: ${inputValue}`, "❌")
     }
   }
 })()
