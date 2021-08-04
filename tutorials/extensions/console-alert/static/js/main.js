f305c654fb331ecc4e46b3fa63301393f7a52b62
Carson <jackparadise520a@gmail.com>
2021-08-04 18:26:20 +0800

    Add example of the ``args``. (#2)

--- tutorials/extensions/console-alert/static/js/main.js
+++ tutorials/extensions/console-alert/static/js/main.js
@@ -4,14 +4,17 @@
       case "show-alert":
         chrome.storage.sync.set({msg: cmdName}) // You can not get the context on the function, so using the Storage API to help you. // https://developer.chrome.com/docs/extensions/reference/storage/
         chrome.tabs.query({active: true, currentWindow: true}).then(([tab])=>{
+          const para = "Hello world!!!"
           chrome.scripting.executeScript({
             target: {tabId: tab.id},
-            function: () => {
+            function: (args) => {
+              alert(args)
               chrome.storage.sync.get(['msg'], ({msg})=> {
                 console.log(`${msg}`)
                 alert(`Command: ${msg}`)
               })
-            }
+            },
+            args: [para]
           })
         })
         break
